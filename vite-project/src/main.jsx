daaf0db7f43774626c48275dc407c5819b07cdc0
Sanidhya Koranne <sanidhyakoranne123@gmail.com>
2022-12-21 15:40:20 +0530

    lifecycle

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -5,11 +5,13 @@ import ReactDOM from 'react-dom/client'
 import Parentfc from './day5/Day5.2/Parent'
 import { HeartFunc } from './day4/day4.2/like2'
 import Todomain from './day6todoappincomponent/todoincomponent'
+import MoviesList from './day7/searchbox'
+import LifeCycleExample from './day7/lifecycle'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Todomain/>
+    <LifeCycleExample/>
     
 
   </React.StrictMode>
